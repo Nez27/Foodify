b47357cf174c37c9b17251ccecc89e9adfbc9c4d
Nezumi <123229563+Nez27@users.noreply.github.com>
2023-02-11 00:28:49 +0700

    Fix UI Slider

--- app/src/main/java/com/capstone/foodify/Fragment/HomeFragment.java
+++ app/src/main/java/com/capstone/foodify/Fragment/HomeFragment.java
@@ -33,13 +33,14 @@ public class HomeFragment extends Fragment {
         imageSlider = (ImageSlider) view.findViewById(R.id.slider);
 
         List<SlideModel> slideModels = new ArrayList<>();
+
         slideModels.add(new SlideModel("https://res.cloudinary.com/dnkmxpujh/image/upload/v1676023773/banner1_dandvd.png", null));
         slideModels.add(new SlideModel("https://res.cloudinary.com/dnkmxpujh/image/upload/v1676023777/banner2_okkqac.png", null));
         slideModels.add(new SlideModel("https://res.cloudinary.com/dnkmxpujh/image/upload/v1676023775/banner3_rklfi6.png", null));
         slideModels.add(new SlideModel("https://res.cloudinary.com/dnkmxpujh/image/upload/v1676023852/banner4_ufsaps.png", null));
         slideModels.add(new SlideModel("https://res.cloudinary.com/dnkmxpujh/image/upload/v1676023778/banner5_b4d1b8.png", null));
 
-        imageSlider.setImageList(slideModels);
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
         // Inflate the layout for this fragment
         return view;
     }
